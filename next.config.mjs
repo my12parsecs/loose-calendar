690a769e7996368cd3bac99b1c113737bb6fe19a
Hikaru <105549697+my12parsecs@users.noreply.github.com>
2024-12-16 10:41:54 +0900

    dynamic locale

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config) => {
+        // Add a rule to handle dynamic imports from the `dayjs/locale` directory
+        config.module.rules.push({
+          test: /dayjs[\\/]locale/,
+          use: 'next-swc-loader',
+          resourceQuery: /lazy/,
+        });
+    
+        // Ensure Webpack doesn't tree-shake required locales
+        config.resolve.alias['dayjs/locale'] = false;
+    
+        return config;
+      },
+};
 
 export default nextConfig;
