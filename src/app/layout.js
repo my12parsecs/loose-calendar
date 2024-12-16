4ec9bfd3f00d02992c5438f45c85936a6e0558fa
Hikaru <105549697+my12parsecs@users.noreply.github.com>
2024-12-16 17:38:55 +0900

    test dvh

--- src/app/layout.js
+++ src/app/layout.js
@@ -33,7 +33,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       {/* <body className={`${geistSans.variable} ${geistMono.variable}`}> */}
-      <body className={`${inter.variable} ${cardo.variable}`}>
+      <body className={`${inter.variable} ${cardo.variable}`} style={{height: "100dvh"}}>
         <Nav />
         {children}
       </body>
