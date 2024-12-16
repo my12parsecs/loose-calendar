8d5f99dc1148445b45474b37a290c80421976e85
Hikaru <105549697+my12parsecs@users.noreply.github.com>
2024-12-16 19:14:05 +0900

    about favicon other

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,13 +1,38 @@
 
+import Link from 'next/link';
 
-import "../stylesheets/page.css";
+import jstz from 'jstimezonedetect';
+import getUserLocale from 'get-user-locale';
+
+import "../stylesheets/about.css";
 
 export default function About() {
 
+    var tz = jstz.determine();
+    const userTimezone = tz.name();
+
+    const userLocale = getUserLocale();
+    const userLanguage = userLocale.slice(0, 2);
     
     return (
-    <div className="main">
-        <h1>About</h1>
+    <div className="about-page">
+        <h1>Loose Calendar for the Week</h1>
+        <h2>is a Calendar where each day has a little memo.</h2>
+        <p>Don't need something fancy and complicated like Google Calendar? Use this!</p>
+        <p>It stores data in your browser's local storage, none of your data is sent to the server. There's no database.</p>
+        <p>I made this in a couple of hours, so please tell me if you find any bugs. Especially the timezone/language stuff. It's supposed to reflect your timezone and language, but I'm not sure if it works on all devices.</p>
+
+        <div>
+            <div>Your Timezone: {userTimezone}</div>
+            <div>Your Language: {userLanguage}</div>
+        </div>
+
+        <div className='go-to-calendar-button-container'>
+            <Link href="/" className='go-to-calendar-button'>Go to Loose Calendar</Link>
+        </div>
+
     </div>
     )
 }
+
+
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,7 @@
-import { Geist, Geist_Mono, Inter, Cardo } from "next/font/google";
+import { Inter, Cardo } from "next/font/google";
 import "./stylesheets/globals.css";
 import Nav from "./components/Nav";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const inter = Inter({
   variable: "--font-inter",
@@ -26,7 +17,7 @@ const cardo = Cardo({
 
 export const metadata = {
   title: "Loose Calendar for the Week",
-  description: "Loose Calendar for the Week",
+  description: "Loose Calendar for the Week is a Calendar where each day has a little memo. You just use it for this week.",
 };
 
 export default function RootLayout({ children }) {
