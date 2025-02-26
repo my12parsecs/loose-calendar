05205df7ceb789e542e93a93971fa9c723a24eb5
Hikaru <105549697+my12parsecs@users.noreply.github.com>
2025-02-26 14:19:52 +0900

    nav css

--- src/app/components/Nav.js
+++ src/app/components/Nav.js
@@ -44,6 +44,8 @@ export default async function Nav() {
                 </div>
             </div>
 
+
+
         </div>
     )
 }
--- src/app/components/ShortcutMenu.jsx
+++ src/app/components/ShortcutMenu.jsx
@@ -34,7 +34,16 @@ export default function ShortcutMenu({which}) {
                     <FontAwesomeIcon icon={faXmark} className="shortcut-icon" />
                 </div>
                 <Link href={"/hotkeys"} className="shortcut-title">Hotkeys</Link>
-                {which === "slug" ? <div className="shortcut-item"><div className="shortcut-key">B</div><div className="shortcut-colon">:</div><div className="shortcut-action">Back to Home</div></div>: null}
+                <div className="shortcut-item"><div className="shortcut-key">B</div><div className="shortcut-colon">:</div><div className="shortcut-action">Back</div></div>
+                <div className="shortcut-item"><div className="shortcut-key">P</div><div className="shortcut-colon">:</div><div className="shortcut-action">Go to Present</div></div>
+                {which !== "slug" ? <div className="shortcut-item"><div className="shortcut-key">SU</div><div className="shortcut-colon">:</div><div className="shortcut-action">Sunday</div></div>: null}
+                {which !== "slug" ? <div className="shortcut-item"><div className="shortcut-key">M</div><div className="shortcut-colon">:</div><div className="shortcut-action">Monday</div></div>: null}
+                {which !== "slug" ? <div className="shortcut-item"><div className="shortcut-key">TU</div><div className="shortcut-colon">:</div><div className="shortcut-action">Tuesday</div></div>: null}
+                {which !== "slug" ? <div className="shortcut-item"><div className="shortcut-key">W</div><div className="shortcut-colon">:</div><div className="shortcut-action">Wednesday</div></div>: null}
+                {which !== "slug" ? <div className="shortcut-item"><div className="shortcut-key">TH</div><div className="shortcut-colon">:</div><div className="shortcut-action">Thursday</div></div>: null}
+                {which !== "slug" ? <div className="shortcut-item"><div className="shortcut-key">F</div><div className="shortcut-colon">:</div><div className="shortcut-action">Friday</div></div>: null}
+                {which !== "slug" ? <div className="shortcut-item"><div className="shortcut-key">SA</div><div className="shortcut-colon">:</div><div className="shortcut-action">Saturday</div></div>: null}
+
                 {which === "slug" ? <div className="shortcut-item"><div className="shortcut-key">L</div><div className="shortcut-colon">:</div><div className="shortcut-action">Next Day</div></div>: null}
                 {which === "slug" ? <div className="shortcut-item"><div className="shortcut-key">H</div><div className="shortcut-colon">:</div><div className="shortcut-action">Previous Day</div></div>: null}
                 {/* {which === "slug" ? <div className="shortcut-item"><div className="shortcut-key">L</div><div className="shortcut-colon">:</div><div className="shortcut-action">Next Week</div></div>: null} */}
@@ -43,7 +52,7 @@ export default function ShortcutMenu({which}) {
                 <div className="shortcut-item"><div className="shortcut-key">J</div><div className="shortcut-colon">:</div><div className="shortcut-action">Previous Week</div></div>
                 {which === "slug" ? <div className="shortcut-item"><div className="shortcut-key">I</div><div className="shortcut-colon">:</div><div className="shortcut-action">Focus</div></div>: null}
                 {which === "slug" ? <div className="shortcut-item"><div className="shortcut-key">Esc</div><div className="shortcut-colon">:</div><div className="shortcut-action">Unfocus</div></div>: null}
-            </div> 
+            </div>
         </div>
     )
 }
