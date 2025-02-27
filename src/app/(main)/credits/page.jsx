37d6c134c86ab0a56e459aa5c5de32bb539a9838
Hikaru <105549697+my12parsecs@users.noreply.github.com>
2025-02-27 23:03:19 +0900

    paper

---
+++ src/app/(main)/credits/page.jsx
@@ -0,0 +1,13 @@
+
+import Link from "next/link";
+
+export default function Credits(){
+
+    return (
+        <div className="page">
+            <h1>Credits</h1>
+            <p>Paper-like background texture</p>
+            <Link href="https://github.com/caph1993/GMeetMono/blob/main/paper-transparent.png" target="_blank">https://github.com/caph1993/GMeetMono/blob/main/paper-transparent.png</Link>
+        </div>
+    )
+}
