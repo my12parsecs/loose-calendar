39e33e91b7a1294e73491df68b74ff3dbbe91a80
Hikaru <105549697+my12parsecs@users.noreply.github.com>
2025-03-01 02:45:31 +0900

    deploy

--- src/app/(main)/credits/page.jsx
+++ src/app/(main)/credits/page.jsx
@@ -6,6 +6,7 @@ export default function Credits(){
     return (
         <div className="page">
             <h1>Credits</h1>
+            
             <p>Paper-like background texture</p>
             <Link href="https://github.com/caph1993/GMeetMono/blob/main/paper-transparent.png" target="_blank">https://github.com/caph1993/GMeetMono/blob/main/paper-transparent.png</Link>
         </div>
